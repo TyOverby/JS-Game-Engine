852099915030e44d68d9d1f938e9e312ab876086
TyOverby <ty@pre-alpha.com>
2011-09-24 17:06:28 -0700

    fixed game startup and update/render code

--- engine.js
+++ engine.js
@@ -2,15 +2,12 @@ function Engine(game){
 	var game = game;
 
 	this.run = function(){
+		game.startup();
 		setInterval(this.update,1);
-		setInterval(this.render,4);
 	}
 
 	this.update = function(){
 		game.update();
-	}
-
-	this.render = function(){
 		game.render();
 	}
 }
