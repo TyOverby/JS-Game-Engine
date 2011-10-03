92f7c547f8ee11e4dd13821dfe869c25f7efbfe5
TyOverby <ty@pre-alpha.com>
2011-10-02 21:07:37 -0700

    added fps limiting

--- draw.js
+++ draw.js
@@ -1,35 +1,44 @@
 function Pane(canvasId){
 	var canvas = document.getElementById(canvasId);
 		if(canvas === undefined){
-				throw "No canvas matching"+canvasId;
+				throw "No canvas matching" + canvasId;
 		}
 	var ctx = canvas.getContext("2d");
 
 	this.getWidth = function(){
-		return ctx.width;
+		return canvas.width;
 	}
 
 	this.getHeight = function(){
-		return ctx.height;
+		return canvas.height;
+	}
+
+	this.getContext = function(type){
+		if(arguments[0]){
+			return canvas.getContext(type);
+		}
+		else{
+			return ctx;
+		}
 	}
 
 
 	this.resetColor = function(){
-		ctx.fillstyle = "cyan";
-		ctx.strokestyle = "cyan";
+		ctx.fillStyle = "cyan";
+		ctx.strokeStyle = "cyan";
 		ctx.globalAlpha = 1;
 		ctx.fillstyle = 5;
 	}
 
 	this.drawRectFill = function(x,y,width,height,color){
-		ctx.fillstyle = color;
+		ctx.fillStyle = color;
 		ctx.fillRect(x,y,width,height);
 
 		this.resetColor();
 	}
 
 	this.drawRectBorder = function(x, y, width, height, color, lineWidth){
-		ctx.fillstyle = color;
+		ctx.fillStyle = color;
 		ctx.lineWidth = lineWidth;
 		ctx.strokeRect(x,y,width,height);
 
--- engine.js
+++ engine.js
@@ -1,13 +1,41 @@
-function Engine(game){
+function Engine(game,fps){
+	var hasRan = false;
 	var game = game;
+	var targetFps = 1000;
+	var prevTime = new Date();
+	
+	if(fps!=undefined&&fps!=null){
+		targetFps = fps;
+	}
+
+	var intervalId;
 
 	this.run = function(){
-		game.startup();
-		setInterval(this.update,1);
+		//only do the startup if the game has ran before
+		if(!hasRan){
+			game.startup();
+		}
+		intervalId = setInterval(this.update,1);
+		hasRan = true;
+	}
+	this.stop = function(message){
+		clearInterval(intervalId);
+		if(message){
+			console.error(message);
+		}
 	}
 
 	this.update = function(){
+		var curTime = new Date();
+
 		game.update();
-		game.render();
+
+		var diffTime=curTime.valueOf()-prevTime.valueOf();
+		if(diffTime>1000/targetFps){
+			game.render();
+			prevTime = curTime;
+		}
+
+		
 	}
 }
